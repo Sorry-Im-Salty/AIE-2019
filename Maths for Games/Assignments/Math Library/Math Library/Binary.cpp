9cdf980be2f40f8e1c3ff480c9d7de9024ce7533
Sorry-Im-Salty <47731654+Sorry-Im-Salty@users.noreply.github.com>
2019-06-04 12:31:13 +1000

    Created functions : int binToDec(const char* binaryString); void decToBin(char* binaryString, int len, int value); void setBit(char& bitfield, char bit, char value); char checkBit(char& bitfield, char bit);

--- Maths for Games/Assignments/Math Library/Math Library/Binary.cpp
+++ Maths for Games/Assignments/Math Library/Math Library/Binary.cpp
@@ -1,9 +1,35 @@
 #include "Binary.h"
+#include <cstring>
+#include <cmath>
 
 Binary::Binary() {
 
 }
 
 Binary::~Binary() {
+
+}
+
+int Binary::binToDec(const char* binaryString) {
+	int dec = 0;
+	int size = strlen(binaryString);
+	int i = 0;
+
+	for (int i = 0; *binaryString; ++i, ++binaryString);
+	{
+		int ch = *binaryString - 48;
+		dec += ch * pow(2, size - i - 1);
+	}
+	return dec;
+}
+
+void Binary::decToBin(char* binaryString, int len, int value) {
+
+}
+
+void Binary::setBit(char& bitfield, char bit, char value) {
+
+}
+char Binary::checkBit(char& bitfield, char bit) {
 
 }
--- Maths for Games/Assignments/Math Library/Math Library/Binary.h
+++ Maths for Games/Assignments/Math Library/Math Library/Binary.h
@@ -1,6 +1,12 @@
 #pragma once
+
 class Binary {
 public:
 	Binary();
 	~Binary();
+
+	int binToDec(const char* binaryString);
+	void decToBin(char* binaryString, int len, int value);
+	void setBit(char& bitfield, char bit, char value);
+	char checkBit(char& bitfield, char bit);
 };
