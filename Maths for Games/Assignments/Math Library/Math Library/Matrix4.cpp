06b1769831891eb46151a975ff74b90c45dd0620
Sorry-Im-Salty <47731654+Sorry-Im-Salty@users.noreply.github.com>
2019-06-04 10:16:25 +1000

    Updated constructors.

--- Maths for Games/Assignments/Math Library/Math Library/Matrix4.cpp
+++ Maths for Games/Assignments/Math Library/Math Library/Matrix4.cpp
@@ -1,14 +1,17 @@
 #include "Matrix4.h"
 
 Matrix4::Matrix4() {
-
+	data[0][0] =	1; 0; 0; 0;
+					0; 1; 0; 0;
+					0; 0; 1; 0;
+					0; 0; 0; 1;
 }
 
-Matrix4::Matrix4(	float m1, float m2, float m3, float m4, 
-					float m5, float m6, float m7, float m8,
-					float m9, float m10, float m11, float m12,
-					float m13, float m14, float m15, float m16	) {
-
+Matrix4::Matrix4(float m1, float m2, float m3, float m4, float m5, float m6, float m7, float m8, float m9, float m10, float m11, float m12, float m13, float m14, float m15, float m16) {
+	data[0][0] =	m1; m2; m3; m4;
+					m5; m6; m7; m8;
+					m9; m10; m11; m12;
+					m13; m14; m15; m16;
 }
 
 Vector4& Matrix4::operator [] (int index) {
