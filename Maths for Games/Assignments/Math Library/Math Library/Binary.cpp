c4f628dca207bee4c82b4477984b7fe7b7cf8ac4
Sorry-Im-Salty <47731654+Sorry-Im-Salty@users.noreply.github.com>
2019-06-11 11:14:13 +1000

    Updated bitfield

--- Maths for Games/Assignments/Math Library/Math Library/Binary.cpp
+++ Maths for Games/Assignments/Math Library/Math Library/Binary.cpp
@@ -15,8 +15,7 @@ int Binary::binToDec(const char* binaryString) {
 	int size = strlen(binaryString);
 	int i = 0;
 
-	for (int i = 0; *binaryString; ++i, ++binaryString);
-	{
+	for (i = 0; *binaryString; ++i, ++binaryString) {
 		int ch = *binaryString - 48;
 		dec += ch * pow(2, size - i - 1);
 	}
@@ -24,12 +23,21 @@ int Binary::binToDec(const char* binaryString) {
 }
 
 void Binary::decToBin(char* binaryString, int len, int value) {
+	int dec = value;
+	int size = strlen(binaryString);
+	int i = len;
+	int a[10];
 
+	for (i = 0; *binaryString; ++i, ++binaryString) {
+		a[i] = dec % 2;
+		dec = dec / 2;
+	}
 }
 
 void Binary::setBit(char& bitfield, char bit, char value) {
-
+	bitfield |= value << bit;
 }
 char Binary::checkBit(char& bitfield, char bit) {
-
+	bit = (bitfield >> bit) & 1U;
+	return bit;
 }
