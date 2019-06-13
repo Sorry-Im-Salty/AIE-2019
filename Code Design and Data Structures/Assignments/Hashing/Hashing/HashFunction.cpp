26bc2b0e336f660750fbf104cbdf42fd432085e2
Sorry-Im-Salty <47731654+Sorry-Im-Salty@users.noreply.github.com>
2019-06-14 09:02:24 +1000

    Added RSHash and JSHash.

--- Code Design and Data Structures/Assignments/Hashing/Hashing/HashFunction.cpp
+++ Code Design and Data Structures/Assignments/Hashing/Hashing/HashFunction.cpp
@@ -11,4 +11,27 @@ namespace HashFunction {
 
 		return hash;
 	}
+
+	unsigned int RSHash(const char* data, unsigned int length) {
+		unsigned int b = 378551;
+		unsigned int a = 63689;
+		unsigned int hash = 0;
+		unsigned int i = 0;
+
+		for (i = 0; i < length; ++data, ++i) {
+			hash = hash * a + (*data);
+			a = a * b;
+		}
+		return hash;
+	}
+
+	unsigned int JSHash(const char* data, unsigned int length) {
+		unsigned int hash = 1315423911;
+		unsigned int i = 0;
+
+		for (i = 0; i < length; ++data, ++i) {
+			hash ^= ((hash << 5) + (*data) + (hash >> 2));
+		}
+		return hash;
+	}
 }
--- Code Design and Data Structures/Assignments/Hashing/Hashing/HashFunction.h
+++ Code Design and Data Structures/Assignments/Hashing/Hashing/HashFunction.h
@@ -5,11 +5,8 @@ namespace HashFunction {
 
 	typedef std::function< unsigned int(const char*, unsigned int)> HashFunc;
 
-	// implementation of a basic addition hash
 	unsigned int badHash(const char* data, unsigned int length);
+	unsigned int RSHash(const char* data, unsigned int length);
+	unsigned int JSHash(const char* data, unsigned int length);
 
-	// ADD YOUR FUNCTIONS HERE
-
-	// a helper to access a default hash function
-	static HashFunc default = badHash;
 }
