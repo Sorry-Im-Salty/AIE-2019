4058cb0168be015a92c68168f7f968d543672837
Sorry-Im-Salty <47731654+Sorry-Im-Salty@users.noreply.github.com>
2019-06-24 12:09:59 +1000

    Added Matrix3 and Vector3 from math lib.

---
+++ Maths for Games/Assignments/aieBootstrap-master/project2D/Matrix3.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Vector3.h"
+#include <cmath>
+
+class Matrix3
+{
+public:
+	Matrix3();
+	Matrix3(	float m1, float m2, float m3,
+				float m4, float m5, float m6, 
+				float m7, float m8, float m9	);
+	Vector3& operator [](int index);
+	const Vector3& operator [](int index) const;
+	operator float*();
+
+	void setRotateX(float x);
+	void setRotateY(float y);
+	void setRotateZ(float z);
+
+	void setScaled(float x, float y, float z);
+	void scale(float x, float y, float z);
+	void translate(float x, float y);
+
+	Matrix3 operator * (const Matrix3& other) const;
+	Vector3 operator * (const Vector3& v) const;
+
+	union {
+		struct {
+			Vector3 xAxis;
+			Vector3 yAxis;
+			Vector3 zAxis;
+		};
+		Vector3 axis[3];
+		float data[3][3];
+	};
+	static const Matrix3 identity;
+};
