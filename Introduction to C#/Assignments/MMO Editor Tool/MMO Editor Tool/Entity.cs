a7fd393fb747e8342abca6fc58949b905c3294f1
Sorry-Im-Salty <47731654+Sorry-Im-Salty@users.noreply.github.com>
2019-08-06 15:34:55 +1000

    Added get and set functions.

--- Introduction to C#/Assignments/MMO Editor Tool/MMO Editor Tool/Entity.cs
+++ Introduction to C#/Assignments/MMO Editor Tool/MMO Editor Tool/Entity.cs
@@ -8,5 +8,115 @@ namespace MMO_Editor_Tool
 {
 	class Entity
 	{
+		private string sType;
+		private string sSubType;
+		private string sName;
+		private int nHealth;
+		private int nSpeed;
+		private int nStrength;
+		private int nRange;
+		private int nMagic;
+		private int nAgility;
+		private int nPrice;
+		private int nLevelReq;
+
+		public void setType(string type)
+		{
+			sType = type;
+		}
+		public string getType()
+		{
+			return sType;
+		}
+
+		public void setSubType(string subType)
+		{
+			sSubType = subType;
+		}
+		public string getSubType()
+		{
+			return sSubType;
+		}
+
+		public void setName(string name)
+		{
+			sName = name;
+		}
+		public string getName()
+		{
+			return sName;
+		}
+
+		public void setHealth(int health)
+		{
+			nHealth = health;
+		}
+		public int getHealth()
+		{
+			return nHealth;
+		}
+
+		public void setSpeed(int speed)
+		{
+			nSpeed = speed;
+		}
+		public int getSpeedh()
+		{
+			return nSpeed;
+		}
+
+		public void setStrength(int strength)
+		{
+			nStrength = strength;
+		}
+		public int getStrength()
+		{
+			return nStrength;
+		}
+
+		public void setRange(int range)
+		{
+			nRange = range;
+		}
+		public int getRange()
+		{
+			return nRange;
+		}
+
+		public void setMagic(int magic)
+		{
+			nMagic = magic;
+		}
+		public int getMagic()
+		{
+			return nMagic;
+		}
+
+		public void setAgility(int agility)
+		{
+			nAgility = agility;
+		}
+		public int getAgility()
+		{
+			return nAgility;
+		}
+
+		public void setPrice(int price)
+		{
+			nPrice = price;
+		}
+		public int getPrice()
+		{
+			return nPrice;
+		}
+
+		public void setLevelReq(int LevelReq)
+		{
+			 nLevelReq = LevelReq;
+		}
+		public int getLevelReq()
+		{
+			return nLevelReq;
+		}
 	}
 }
