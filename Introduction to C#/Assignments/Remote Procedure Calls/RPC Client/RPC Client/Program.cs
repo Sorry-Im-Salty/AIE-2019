a973fa2ec1804fe5a7fc14979a6028eb50996e33
Sorry-Im-Salty <47731654+Sorry-Im-Salty@users.noreply.github.com>
2019-08-12 10:30:42 +1000

    Completed Project.

--- Introduction to C#/Assignments/Remote Procedure Calls/RPC Client/RPC Client/Program.cs
+++ Introduction to C#/Assignments/Remote Procedure Calls/RPC Client/RPC Client/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting.Channels;
 
-namespace RPC_Client
+class Program
 {
-	class Program
+	static void Main(string[] args)
 	{
-		static void Main(string[] args)
+		// Create Client
+		TcpClientChannel channel = new TcpClientChannel();
+		ChannelServices.RegisterChannel(channel, false);
+
+		// Get a reference to the player on the server
+		string playerURL = "tcp://localhost:" + 4269 + "/" + "Player";
+		Player player = (Player)Activator.GetObject(typeof(Player), playerURL);
+
+		while (true)
 		{
+			Console.Write("Type a message to the server or type 'quit' to exit\n");
+			string text = Console.ReadLine();
+
+			if (text == "quit")
+				break;
+
+			// RPC: Call function on server
+			player.SayHello(text);
 		}
 	}
 }
--- Introduction to C#/Assignments/Remote Procedure Calls/RPC Server/RPC Server/Program.cs
+++ Introduction to C#/Assignments/Remote Procedure Calls/RPC Server/RPC Server/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting.Channels;
 
-namespace RPC_Server
+class Program
 {
-	class Program
+	static void Main(string[] args)
 	{
-		static void Main(string[] args)
-		{
-		}
+		// Create Server
+		TcpServerChannel channel = new TcpServerChannel(4269);
+		ChannelServices.RegisterChannel(channel, false);
+
+		// Register Player
+		RemotingConfiguration.RegisterWellKnownServiceType(typeof(Player), "Player", WellKnownObjectMode.SingleCall);
+
+		Console.WriteLine("Listening to requests. Press enter to exit...");
+		Console.ReadLine();
+
 	}
 }
